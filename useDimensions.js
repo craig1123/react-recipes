d3555c3086f9984fa25bba6f332edf7ca0944829
craig walker <craigwalker1123@gmail.com>
2019-10-25 09:42:32 -0600

    useDimensions hooks and debounce util

---
+++ useDimensions.js
@@ -0,0 +1,37 @@
+import { useState, useCallback, useLayoutEffect } from 'react';
+import debounce from './utils/debounce';
+
+function useDimensions({ liveMeasure: liveMeasure = true, timer: timer = 250 }) {
+  const [dimensions, setDimensions] = useState({});
+  const [node, setNode] = useState(null);
+
+  const ref = useCallback(newNode => {
+    setNode(newNode);
+  }, []);
+
+  useLayoutEffect(() => {
+    if (node) {
+      const measure = debounce(() => {
+        window.requestAnimationFrame(() => {
+          const newDimensions = node.getBoundingClientRect();
+          setDimensions(newDimensions);
+        });
+      }, timer);
+      measure();
+
+      if (liveMeasure) {
+        window.addEventListener('resize', measure);
+        window.addEventListener('scroll', measure);
+
+        return () => {
+          window.removeEventListener('resize', measure);
+          window.removeEventListener('scroll', measure);
+        };
+      }
+    }
+  }, [node, liveMeasure]);
+
+  return [ref, dimensions, node];
+}
+
+export default useDimensions;
---
+++ utils/debounce.js
@@ -0,0 +1,25 @@
+// https://davidwalsh.name/javascript-debounce-function
+
+function debounce(func, wait, immediate) {
+  let timeout;
+
+  return function executedFunction(...args) {
+    const context = this;
+
+    // eslint-disable-next-line func-names
+    const later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+
+    const callNow = immediate && !timeout;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(later, wait);
+
+    if (callNow) func.apply(context, args);
+  };
+}
+
+export default debounce;
