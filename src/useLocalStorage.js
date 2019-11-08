e026a06251d5272933e8b008ba7022f5ca094a9b
donavon <github@donavon.com>
2019-11-08 16:58:19 -0500

    fix bug in useLocalStorage
    if the previously saved value was falsy, you were returning the default value. this would happen for empty string, zero, and `false`. i changed this to compare for `null` (i.e. the value for no key found).

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -8,7 +8,7 @@ function useLocalStorage(key, initialValue) {
       // Get from local storage by key
       const item = window.localStorage.getItem(key);
       // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue;
+      return item !== null ? JSON.parse(item) : initialValue;
     } catch (error) {
       // If error also return initialValue
       console.log(error);
